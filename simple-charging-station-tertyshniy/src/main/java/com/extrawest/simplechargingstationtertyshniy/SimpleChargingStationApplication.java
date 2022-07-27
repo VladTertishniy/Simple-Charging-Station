7bd5e9065208fc2849fc46e7ae8be16030164002
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-27 23:42:21 +0300

    package refactoring

--- simple-charging-station-tertyshniy/src/main/java/com/extrawest/simplechargingstationtertyshniy/SimpleChargingStationApplication.java
+++ simple-charging-station-tertyshniy/src/main/java/com/extrawest/simplechargingstationtertyshniy/SimpleChargingStationApplication.java
@@ -1,4 +1,4 @@
-package com.etrawest.simplechargingstationtertyshniy;
+package com.extrawest.simplechargingstationtertyshniy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- simple-charging-station-tertyshniy/src/test/java/com/extrawest/simplechargingstationtertyshniy/SimpleChargingStationyApplicationTests.java
+++ simple-charging-station-tertyshniy/src/test/java/com/extrawest/simplechargingstationtertyshniy/SimpleChargingStationyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.etrawest.simplechargingstationtertyshniy;
+package com.extrawest.simplechargingstationtertyshniy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
