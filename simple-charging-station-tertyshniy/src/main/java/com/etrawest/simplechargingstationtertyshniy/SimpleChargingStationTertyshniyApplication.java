9ddf36fcf89fd847d30fd0001909576168c5b0c4
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-27 18:57:05 +0300

    empty project

---
+++ simple-charging-station-tertyshniy/src/main/java/com/etrawest/simplechargingstationtertyshniy/SimpleChargingStationTertyshniyApplication.java
@@ -0,0 +1,13 @@
+package com.etrawest.simplechargingstationtertyshniy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChargingStationTertyshniyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChargingStationTertyshniyApplication.class, args);
+	}
+
+}
