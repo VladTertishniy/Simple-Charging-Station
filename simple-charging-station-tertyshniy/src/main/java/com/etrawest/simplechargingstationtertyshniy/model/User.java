33a2a39af0f1ab94bf8cb04996e9a7f0e12495a4
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-27 22:53:20 +0300

    added User.java model + needed dependencies

---
+++ simple-charging-station-tertyshniy/src/main/java/com/etrawest/simplechargingstationtertyshniy/model/User.java
@@ -0,0 +1,25 @@
+package com.etrawest.simplechargingstationtertyshniy.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private long phoneNumber;
+
+    protected User(String email, String password, long phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+}
