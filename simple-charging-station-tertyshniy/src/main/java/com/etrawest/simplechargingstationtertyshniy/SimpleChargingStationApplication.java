a86daca5c59641437613e40c6404435cb40024d1
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-27 22:50:42 +0300

    added needed dependencies

--- simple-charging-station-tertyshniy/src/main/java/com/etrawest/simplechargingstationtertyshniy/SimpleChargingStationApplication.java
+++ simple-charging-station-tertyshniy/src/main/java/com/etrawest/simplechargingstationtertyshniy/SimpleChargingStationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleChargingStationTertyshniyApplication {
+public class SimpleChargingStationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleChargingStationTertyshniyApplication.class, args);
+		SpringApplication.run(SimpleChargingStationApplication.class, args);
 	}
 
 }
