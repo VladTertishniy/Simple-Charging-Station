5aa402d81dc6ea07343f4ba51ca4918be287e8a1
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-27 23:45:39 +0300

    added GlobalExceptionHandler.java and ApiRequestException.java

---
+++ simple-charging-station-tertyshniy/src/main/java/com/extrawest/simplechargingstationtertyshniy/exception/ApiRequestException.java
@@ -0,0 +1,7 @@
+package com.extrawest.simplechargingstationtertyshniy.exception;
+
+public class ApiRequestException extends RuntimeException {
+    public ApiRequestException(String message) {
+        super(message);
+    }
+}
---
+++ simple-charging-station-tertyshniy/src/main/java/com/extrawest/simplechargingstationtertyshniy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.extrawest.simplechargingstationtertyshniy.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ NoSuchElementException.class})
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler({ ApiRequestException.class})
+    public ResponseEntity<String> handleApiRequestException(ApiRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
